ab8cf5fb336240c99883175a54b0266512aa7db2
manggury <148739312+manggury@users.noreply.github.com>
2024-03-14 22:49:08 +0900

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,4 +1,5 @@
 $(function () {
+
     $('.utils_wrap').on('click', function () {
         $('.gnb').toggleClass('on')
     });
@@ -8,6 +9,8 @@ $(function () {
     }).on('mouseleave', function () {
         $(this).toggleClass('on');
     });
+
+
 
     $('a[href^="#"]').on('click', function (event) {
         var target = $(this.getAttribute('href'));
@@ -23,4 +26,16 @@ $(function () {
         $('html, body').animate({ scrollTop: 0 }, 800);
         return false;
     });
+
+    // const hd = gsap.timeline({
+    //     pin: true,
+    // });
+    // hd.to(".inner", { y: 80, delay: 3, })
+
+    const mainVisual = gsap.timeline();
+    mainVisual.to(".my_slogan .rotate_txt", { rotateX: 360, speeds: 5000, duration: 2, })
+    mainVisual.to(".my_slogan .rotate_txt02", { rotateX: 0, speeds: 5000, duration: 2, })
+    mainVisual.to(".mainVisal .my_slogan_sub", { opacity: 1, duration: 1, })
+    mainVisual.to(".mainVisal .line", { width: 1680, duration: 1, })
+
 });
